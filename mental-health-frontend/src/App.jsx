57ab5bd88e8118e306ef8aa92a8c7d2a3dfbab95
20py03 <jovana.papric2@gmail.com>
2026-01-22 17:41:32 +0100

    Update frontend for deploy

--- mental-health-frontend/src/App.jsx
+++ mental-health-frontend/src/App.jsx
@@ -144,7 +144,7 @@ export default function App() {
     setResult(null);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/predict", {
+      const res = await fetch("https://mental-health-risk-prediction-qd0t.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form)
